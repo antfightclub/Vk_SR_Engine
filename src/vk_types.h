c04f4722918b8215209948060dafc9cf8f83f574
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-12 18:09:05 +0200

    Fill out vk_types.h for initial work

--- src/vk_types.h
+++ src/vk_types.h
@@ -1,1 +1,28 @@
+#pragma once
 //vk_types.h
+
+// Standard library includes for various functionality across the engine (bite me)
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+#include <span>
+#include <array>
+#include <functional>
+#include <deque>
+
+// Vulkan includes
+#include <vulkan/vulkan.hpp> // using the HPP bindings
+#include <vulkan/vk_enum_string_helper.h>
+
+
+
+// Macro to check Vulkan error codes to eas use of most vulkan functions
+#define VK_CHECK(x)															\
+do {																		\
+	VkResult err = x;														\
+	if (err) {																\
+		fmt::println("Detected Vulkan error: {}", string_Vkresult(err));	\
+			abort();														\
+	}																		\
+	while (0)
