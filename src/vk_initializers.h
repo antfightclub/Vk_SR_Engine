e3f14f90ed1bc7dab7beb8f62921624d6ad0df56
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-13 19:39:42 +0200

    add stubs for command pool and buffer info structs

--- src/vk_initializers.h
+++ src/vk_initializers.h
@@ -3,9 +3,15 @@
 
 #include <vk_types.h>
 
-namespace vkinit {
-	//> images
+namespace vkinit 
+{
+	//> init_cmds
+	vk::CommandPoolCreateInfo command_pool_create_info(uint32_t queueFamilyIndex, vk::CommandPoolCreateFlags flags = 0);
+	vk::CommandBufferAllocateInfo command_buffer_allocate_info(vk::CommandPool pool, uint32_t count = 1);
+	//< init_cmds
+
+	//> init_images
 	vk::ImageCreateInfo image_create_info(vk::Format format, vk::ImageUsageFlags usageFlags, vk::Extent3D extent);
 	vk::ImageViewCreateInfo imageview_create_info(vk::Format format, vk::Image image, vk::ImageAspectFlags aspectFlags);
-	//< images
+	//< init_images
 }
