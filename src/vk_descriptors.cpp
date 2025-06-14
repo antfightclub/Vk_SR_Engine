e9194554141e090cf1ee97775f1962bf8781be6a
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-14 16:58:30 +0200

    implemented DescriptorLayoutBuilder

--- src/vk_descriptors.cpp
+++ src/vk_descriptors.cpp
@@ -1,1 +1,44 @@
 #include "vk_descriptors.h"
+
+//> DescriptorLayoutBuilder
+void DescriptorLayoutBuilder::add_binding(uint32_t binding, vk::DescriptorType type) {
+	vk::DescriptorSetLayoutBinding newbind = {};
+	newbind.binding = binding;
+	newbind.descriptorCount = 1;
+	newbind.descriptorType = type;
+
+	bindings.push_back(newbind);
+}
+
+void DescriptorLayoutBuilder::clear() {
+	bindings.clear();
+}
+
+vk::DescriptorSetLayout DescriptorLayoutBuilder::build(vk::Device device, vk::ShaderStageFlags shaderStages, void* pNext = nullptr, vk::DescriptorSetLayoutCreateFlags flags) {
+	for (auto& b : bindings) {
+		b.stageFlags |= shaderStages;
+	}
+
+	vk::DescriptorSetLayoutCreateInfo info = {};
+	info.pNext = pNext;
+
+	info.pBindings = bindings.data();
+	info.bindingCount = (uint32_t)bindings.size();
+	info.flags = flags;
+
+	vk::DescriptorSetLayout set;
+	VK_CHECK(device.createDescriptorSetLayout(&info, nullptr, &set));
+}
+//< DescriptorLayoutBuilder
+
+//> DescriptorAllocator
+
+//< DescriptorAllocator
+
+//> DescriptorAllocatorGrowable
+
+//< DescriptorAllocatorGrowable
+
+//> DescriptorWriter
+
+//< DescriptorWriter
