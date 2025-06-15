73ba6e1684deb0cf73e35ff48f0d5ef39927febd
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-15 20:36:14 +0200

    added geometry structures to vk_types

--- src/vk_types.h
+++ src/vk_types.h
@@ -35,7 +35,7 @@ do {																		\
 		}																	\
 } while (0)
 
-
+//> allocated
 struct AllocatedImage {
 	vk::Image image;
 	vk::ImageView imageView;
@@ -43,3 +43,87 @@ struct AllocatedImage {
 	vk::Extent3D imageExtent;
 	vk::Format imageFormat;
 };
+
+struct AllocatedBuffer {
+	vk::Buffer buffer;
+	vma::Allocation allocation;
+	vma::AllocationInfo info;
+};
+//< allocated
+
+//> mesh
+struct Vertex {
+	glm::vec3 postion;
+	float uv_x;
+	glm::vec3 normal;
+	float uv_y;
+	glm::vec4 color;
+};
+
+struct GPUMeshBuffers {
+	AllocatedBuffer indexBuffer;
+	AllocatedBuffer vertexBuffer;
+	vk::DeviceAddress vertexBufferAddress;
+};
+
+struct GPUDrawPushConstants {
+	glm::mat4 worldMatrix;
+	vk::DeviceAddress vertexBuffer;
+};
+//< mesh
+
+//> material
+enum class MaterialPass : uint8_t {
+	MainColor,
+	Transparent,
+	Other
+};
+
+struct MaterialPipeline {
+	vk::Pipeline pipeline;
+	vk::Pipeline layout;
+};
+
+struct MaterialInstance {
+	MaterialPipeline* pipeline;
+	vk::DescriptorSet materialSet;
+	MaterialPass passType;
+};
+//< material
+
+// Forward declaration for DrawContext 
+struct DrawContext;
+
+//> renderables
+// Base class for a renderable dynamic object
+class IRenderable {
+	virtual void Draw(const glm::mat4& topMatrix, DrawContext& ctx) = 0;
+};
+
+// Implementation of a drawable scene node
+// The scene node can hold children and will also keep a transform to propagate to them
+struct Node : public IRenderable {
+
+	// Parent pointer must be a weak pointer to avoid circular dependencies
+	std::weak_ptr<Node> parent;
+	std::vector<std::shared_ptr<Node>> children;
+
+	glm::mat4 localTransform;
+	glm::mat4 worldTransform;
+
+	void refreshTransform(const glm::mat4& parentMatrix) {
+		worldTransform = parentMatrix * localTransform;
+		for (auto c : children) {
+			c->refreshTransform(worldTransform);
+		}
+	}
+
+	virtual void Draw(const glm::mat4& topMatrix, DrawContext& ctx) {
+		// Draw children
+		for (auto& c : children) {
+			c->Draw(topMatrix, ctx);
+		}
+	}
+};
+
+//< renderables
