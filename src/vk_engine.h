a338272cbd8431e6f053c3b3e45c5af47a170ccf
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-15 20:12:36 +0200

    add GPUSceneData struct

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -47,6 +47,15 @@ struct FrameData
 	DescriptorAllocatorGrowable _frameDescriptors;
 };
 
+struct GPUSceneData {
+	glm::mat4 view;
+	glm::mat4 proj;
+	glm::mat4 viewproj;
+	glm::vec4 ambientColor;
+	glm::vec4 sunlightDirection; // w for sun power
+	glm::vec4 sunlightColor;
+};
+
 class VkSREngine {
 public:
 	bool _isInitialized{ false };
