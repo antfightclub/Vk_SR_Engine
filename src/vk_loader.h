1dcbef2b0635846dd342a0bd4f02cd12f21f4415
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-15 20:48:58 +0200

    added geometry related structures

--- src/vk_loader.h
+++ src/vk_loader.h
@@ -13,3 +13,26 @@ struct GLTFMaterial {
 	MaterialInstance data;
 };
 //< material
+
+//> mesh
+struct Bounds {
+	glm::vec3 origin;
+	float sphereRadius;
+	glm::vec3 extents;
+};
+
+struct GeoSurface {
+	uint32_t startIndex;
+	uint32_t count;
+	Bounds bounds;
+	std::shared_ptr<GLTFMaterial> material;
+};
+
+struct MeshAsset {
+	std::string name;
+
+	std::vector<GeoSurface> surfaces;
+	GPUMeshBuffers meshBuffers;
+};
+//< mesh
+
