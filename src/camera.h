109d355079998e850bc4e9d5fd74787bd29644da
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-15 20:18:38 +0200

    added camera.h sources and struct

---
+++ src/camera.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vk_types.h>
+#include <SDL3/SDL_events.h>
+
+class Camera {
+public: 
+	glm::vec3 velocity;
+	glm::vec3 position;
+
+	// Vertical rotation 
+	float pitch{ 0.f };
+
+	// Horizontal rotation
+	float yaw{ 0.f };
+
+	glm::mat4 getViewMatrix();
+	glm::mat4 getRotationMatrix();
+
+	void processSDLEvent(SDL_Event& e);
+
+	void update();
+};
