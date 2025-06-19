cc3a7b905ee708ed0f13abc7e8af5b3f86815a5e
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-19 16:43:45 +0200

    zero-initialize some members

--- src/compute_structs.h
+++ src/compute_structs.h
@@ -3,16 +3,16 @@
 #include "vk_types.h"
 
 struct ComputePushConstants {
-	glm::vec4 data1;
-	glm::vec4 data2;
-	glm::vec4 data3;
-	glm::vec4 data4;
+	glm::vec4 data1{};
+	glm::vec4 data2{};
+	glm::vec4 data3{};
+	glm::vec4 data4{};
 };
 
 struct ComputeEffect {
-	const char* name;
+	const char* name = "default";
 	vk::Pipeline pipeline;
 	vk::PipelineLayout layout;
 
-	ComputePushConstants data;
+	ComputePushConstants data{};
 };
