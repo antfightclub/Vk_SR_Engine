54a47f308bf87ef7d80e707c3a05c64477816c0f
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-19 19:33:07 +0200

    implement MeshNode::Draw()

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -883,5 +883,25 @@ MaterialInstance GLTFMetallic_Roughness::write_material(vk::Device device, Mater
 
 // ############## MeshNode ###############
 void MeshNode::Draw(const glm::mat4& topMatrix, DrawContext& ctx) {
-	
+	glm::mat4 nodeMatrix = topMatrix * worldTransform;
+
+	for (auto& s : mesh->surfaces) {
+		RenderObject def;
+		def.indexCount = s.count;
+		def.firstIndex = s.startIndex;
+		def.indexBuffer = mesh->meshBuffers.indexBuffer.buffer;
+		def.material = &s.material->data;
+		def.bounds = s.bounds;
+		def.vertexBufferAddress = mesh->meshBuffers.vertexBufferAddress;
+
+		if (s.material->data.passType == MaterialPass::Transparent) {
+			ctx.TransparentSurfaces.push_back(def);
+		}
+		else {
+			ctx.OpaqueSurfaces.push_back(def);
+		}
+	}
+
+	// Recurse down
+	Node::Draw(topMatrix, ctx);
 }
