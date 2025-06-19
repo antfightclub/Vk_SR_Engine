bb5081c55ee194e1e590f267d53a528734487b3c
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-19 11:25:19 +0200

    addded stubs for global funcs and member funcs

--- src/vk_loader.cpp
+++ src/vk_loader.cpp
@@ -12,3 +12,33 @@
 #include <fastgltf/core.hpp>
 #include <fastgltf/tools.hpp>
 #include <fastgltf/util.hpp>
+
+//> global_funcs
+std::optional<AllocatedImage> load_image(VulkanEngine* engine, fastgltf::Asset& asset, fastgltf::Image& image) {
+	// Nothing yet
+}
+
+vk::Filter extract_filter(fastgltf::Filter filter) {
+	// Nothing yet
+}
+
+vk::SamplerMipmapMode extract_mipmap_mode(fastgltf::Filter filter) {
+	// Nothing yet
+}
+//< global_funcs
+
+//> loadgltf_func
+std::optional<std::shared_ptr<LoadedGLTF>> loadGltf(VulkanEngine* engine, std::string_view filePath) {
+
+}
+//< loadgltf_func
+
+//> LoadedGLTF
+void LoadedGLTF::Draw(const glm::mat4& topMatrix, DrawContext& ctx) {
+	// Nothing yet
+}
+
+void LoadedGLTF::clearAll() {
+	// Nothing yet
+}
+//< LoadedGLTF
