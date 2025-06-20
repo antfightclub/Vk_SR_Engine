f418de26c70dd8a75fd12d3273b14cec6dbf7310
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-20 15:42:46 +0200

    implement is_visible() to cull object draws

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -574,6 +574,50 @@ void VkSREngine::cleanup()
 //< cleanup
 
 //> draw
+bool is_visible(const RenderObject& obj, const glm::mat4& viewProj) {
+	// Simple culling function 
+	// Define the 8 corners of the normalized device coordinates (NDC) or clip space, the space objects are in after view and projection
+	std::array<glm::vec3, 8> corners{
+		glm::vec3 { 1, 1, 1 },
+		glm::vec3 { 1, 1, -1 },
+		glm::vec3 { 1, -1, 1 },
+		glm::vec3 { 1, -1, -1 },
+		glm::vec3 { -1, 1, 1 },
+		glm::vec3 { -1, 1, -1 },
+		glm::vec3 { -1, -1, 1 },
+		glm::vec3 { -1, -1, -1 },
+	};
+
+	// Get object's position in NDC
+	glm::mat4 matrix = viewProj * obj.transform;
+
+	// Min and max bounds as the two corners of a box
+	glm::vec3 min = { 1.5, 1.5, 1.5 };
+	glm::vec3 max = { -1.5, -1.5, -1.5 };
+
+	for (int c = 0; c < 8; c++) {
+		// Project each corner into clip space
+		glm::vec4 v = matrix * glm::vec4(obj.bounds.origin + (corners[c] * obj.bounds.extents), 1.f);
+
+		// Perspective correction
+		v.x = v.x / v.w;
+		v.y = v.y / v.w;
+		v.z = v.z / v.w;
+
+		min = glm::min(glm::vec3{ v.x, v.y, v.z }, min);
+		max = glm::max(glm::vec3{ v.x, v.y, v.z }, max);
+	}
+
+	// Check whether the clip space box is within view
+	if (min.z > 1.f || max.z < 0.f || min.x > 1.f || max.x < -1.f || min.y > 1.f || max.y < -1.f) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+
 void VkSREngine::draw() {
 	// Wait until the GPU has finished rendering the last frame. Timout of 1 second
 	VK_CHECK(_device.waitForFences(1, &get_current_frame()._renderFence, true, 1000000000));
